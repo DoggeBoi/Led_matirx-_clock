2ed93a1304e2a762eaa23d11ea1304764c9aa93d
DoggeBoi <dogge03@gmail.com>
2019-12-23 18:09:41 +0000

    3

--- test1.py
+++ test1.py
@@ -1,4 +1,10 @@
-print('hello world')
+import requests
+from pprint import pprint
 
-print("yolo")
+url = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,se&appid=68b462ca1a175b9178635fb70dac6848'
 
+res = requests.get(url)
+
+data = res.json()
+
+pprint(data)
